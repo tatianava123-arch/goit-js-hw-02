62c5db5ba4a68665f42e66770dc1f04e4e50d5fc
tatanavasilcova <tetiana.vasyltsova@gen.tech>
2026-04-21 00:28:33 +0300

    fix formatting

--- js/task-1.js
+++ js/task-1.js
@@ -2,7 +2,7 @@ function makeTransaction(quantity, pricePerDroid, customerCredits) {
   const totalPrice = quantity * pricePerDroid;
 
   if (totalPrice > customerCredits) {
-    return "Insufficient funds!";
+    return 'Insufficient funds!';
   }
 
   return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
@@ -12,4 +12,4 @@ console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 150
 console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
 console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
 console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
-console.log(makeTransaction(10, 500, 5000));  // "You ordered 10 droids worth 5000 credits!"
+console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
